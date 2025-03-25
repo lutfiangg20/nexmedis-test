0d621676f5a7d853c1937eb3f8a0eaa0ec1b6c58
lutfiangg20 <lutfiangg20@gmail.com>
2025-03-25 14:52:37 +0700

    Inital commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="flex justify-center items-center  min-h-screen w-full">
+    <div class="shadow-2xl">
+      <div class="flex flex-col gap-2 px-10 py-5">
+        <h1 class="capitalize">login</h1>
+        <input placeholder="username" />
+        <input placeholder="password" />
+        <button class="capitalize bg-indigo-500">login</button>
+      </div>
+    </div>
+  </div>
+</template>
