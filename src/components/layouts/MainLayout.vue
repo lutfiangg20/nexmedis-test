67b4e1ba6324d594b74c28a52f0be32814f050df
lutfiangg20 <lutfiangg20@gmail.com>
2025-03-26 15:25:47 +0700

    add homepage

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Sidebar from './Sidebar.vue'
+import { Columns2, Menu } from 'lucide-vue-next'
+</script>
+<template>
+  <div class="flex">
+    <Sidebar />
+    <div class="w-full">
+      <header class="py-4 px-2 h-16 flex justify-between border-b border-gray-200">
+        <button><Columns2 /></button>
+        <div>
+          <button><Menu /></button>
+        </div>
+      </header>
+      <div class="p-5 w-full min-h-screen">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/layouts/Sidebar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+type Data = {
+  name: string
+  url: string
+}
+const datas: Data[] = [
+  {
+    name: 'Home',
+    url: '/home',
+  },
+  {
+    name: 'Profile',
+    url: '/profile',
+  },
+]
+</script>
+<template>
+  <aside class="w-60 border-r border-gray-200">
+    <div class="px-5 py-4 h-16 border-b border-gray-200">
+      <h1 class="text-2xl font-bold">Dashboard</h1>
+    </div>
+    <ul class="px-5">
+      <li
+        class="cursor-pointer hover:bg-gray-200 rounded-md p-2"
+        v-for="(item, index) in datas"
+        :key="index"
+      >
+        <router-link :to="item.url">{{ item.name }}</router-link>
+      </li>
+    </ul>
+  </aside>
+</template>
